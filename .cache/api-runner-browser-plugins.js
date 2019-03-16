99cd7839839e4ca56681ec77d3c03a044a211b3c
Crystal Barragán <crystalbarragan@icloud.com>
2019-03-15 23:07:18 -0500

    Deploy site

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/crystalbarragan/GitHub/gatsby-test/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/crystalbarragan/GitHub/gatsby-test/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
